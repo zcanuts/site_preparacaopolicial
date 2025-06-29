56b500289cf85228c047a2db676361e4f5c03682
Seu Nome <zaramelloc@gmail.com>
2025-06-29 14:27:32 -0300

    Deploy

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
 export class PricingComponent {
-
+  // Controla se os planos devem ser exibidos (false = lançamento gratuito)
+  showPricing: boolean = false;
 }
